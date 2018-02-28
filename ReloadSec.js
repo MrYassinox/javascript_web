4fae84ac7609a44a6d41daa1b5b504367a2ab0f6
Yassin <36928873+Yassinox-Dz@users.noreply.github.com>
2018-02-28 18:51:39 +0100

    Create ReloadSec.js

---
+++ ReloadSec.js
@@ -0,0 +1,34 @@
+  var seconds;
+  var temp;
+function removem1() {
+    var xkl = getQueryVariable("url");
+    var nkl=xkl.replace("&m=1", "");
+document.getElementById("reload").innerHTML=nkl;
+}
+removem1();
+document.getElementById('reload').href=window.location.href;
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("?&&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=_");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+  function countdown() {
+    seconds = document.getElementById('countdown').innerHTML;
+    seconds = parseInt(seconds, 10);
+
+    if (seconds == 0) {
+      temp = document.getElementById('countdown');
+      parent.location.replace(document.getElementById("reload").innerHTML)
+           return;
+    }
+    seconds--;
+    temp = document.getElementById('countdown');
+    temp.innerHTML = seconds;
+    timeoutMyOswego = setTimeout(countdown, 500);
+  }
+setTimeout(function(){ countdown(); }, 10000);
